e2e5bfe142ffc454e11a9ce761eb959bb027ff92
moinul-islam <usermoin@gmail.com>
2025-10-24 13:21:13 +0600

    udpad

--- resources/views/frontend/body/review-modal.blade.php
+++ resources/views/frontend/body/review-modal.blade.php
@@ -3,9 +3,9 @@
                   <div class="modal-dialog modal-dialog-scrollable">
                       <div class="modal-content">
                           <div class="modal-header">
-                              <h5 class="modal-title">
-                                  {{ $isUserProfile ? 'Reviews for ' . $item->name : 'Reviews for ' . $item->title }}
-                              </h5>
+                          <h5 class="modal-title">
+                                {{ isset($item->name) ? 'Reviews for ' . $item->name : 'Reviews for ' . $item->title }}
+                            </h5>
                               <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
                           </div>
                           <div class="modal-body">
