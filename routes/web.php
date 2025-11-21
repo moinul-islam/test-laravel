3bf271d2b68d3a2fbacde5e5d620151eb8a8a49f
Moinul Islam <usermoin@gmail.com>
2025-11-21 22:56:13 +0600

    ebar mark as a seen bad

--- routes/web.php
+++ routes/web.php
@@ -191,7 +191,7 @@ Route::post('/account-check', [ProfileController::class, 'accountCheck'])->name(
 // Notification routes
 Route::middleware('auth')->group(function () {
     Route::get('/notifications', [NotificationController::class, 'index'])->name('notifications.index');
-    Route::post('/notifications/mark-as-seen', [NotificationController::class, 'markAsSeen'])->name('notifications.markAsSeen');
+    // Route::post('/notifications/mark-as-seen', [NotificationController::class, 'markAsSeen'])->name('notifications.markAsSeen');
     Route::post('/notifications/mark-all-seen', [NotificationController::class, 'markAllAsSeen'])->name('notifications.markAllSeen');
     Route::get('/notifications/unseen-count', [NotificationController::class, 'getUnseenCount'])->name('notifications.unseenCount');
     Route::delete('/notifications/delete', [NotificationController::class, 'destroy'])->name('notifications.delete');
